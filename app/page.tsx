369fad960a046689edf4752a747dcab5f99c9d96
Quytkiemsy <hanhat123123@gmail.com>
2025-04-12 23:09:32 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -50,6 +51,12 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+        <Link href="/product">
+          <div className="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Go to Product Page
+          </div>
+        </Link>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
